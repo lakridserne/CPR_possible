8b6a3f3267a1ff80b50bf032c50800000921d1c1
Kristoffer Rath Hansen <kristoffer@codingpirates.dk>
2018-02-24 21:12:41 +0100

    Add simple version for teaching kids in Coding Pirates

---
+++ cpr_validator.py
@@ -0,0 +1,22 @@
+# This is a simple version of cpr_generator.php used to teach kids in Coding
+# Pirates how this works. It just validates the CPR number instead of generating a list
+
+cpr_num = "1902075921"
+gender = "boy"
+
+# multiply by control chiffers
+multiplied = []
+control_numbers = [4,3,2,7,6,5,4,3,2]
+i = 0
+for cpr in cpr_num:
+    if i < 9:
+        multiplied.append(int(cpr) * control_numbers[i])
+        i = i + 1
+
+sum_cpr = sum(multiplied)
+rest = sum_cpr % 11
+last_num = 11-rest
+if int(last_num) == int(cpr_num[9]):
+    print("CPR " + cpr_num + " is valid")
+else:
+    print("CPR " + cpr_num + " is not valid")
